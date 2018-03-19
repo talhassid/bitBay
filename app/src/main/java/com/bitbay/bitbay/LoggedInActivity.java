8c12a1e2171a98961e0aab8ec3eeeabf68eedd41
talhassid <talhassidd@gmail.com>
2018-03-19 13:32:31 +0200

    remove the fregmant activities

--- app/src/main/java/com/bitbay/bitbay/LoggedInActivity.java
+++ app/src/main/java/com/bitbay/bitbay/LoggedInActivity.java
@@ -1,18 +1,10 @@
 package com.bitbay.bitbay;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class LoggedInActivity extends AppCompatActivity{
--- app/src/main/java/com/bitbay/bitbay/SignInActivity.java
+++ app/src/main/java/com/bitbay/bitbay/SignInActivity.java
@@ -1,10 +1,10 @@
 package com.bitbay.bitbay;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
