915c5a694eb08be001c402faa796873e7a084832
roeisun <roeisun77@gmail.com>
2018-03-19 23:12:06 +0200

    add profile activity and log massage to menu buttons

--- app/src/main/java/com/bitbay/bitbay/LoggedInActivity.java
+++ app/src/main/java/com/bitbay/bitbay/LoggedInActivity.java
@@ -1,13 +1,16 @@
 package com.bitbay.bitbay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.util.Log;
 
 
-public class LoggedInActivity extends AppCompatActivity{
+
+public class LoggedInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,25 +19,35 @@ public class LoggedInActivity extends AppCompatActivity{
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu,menu);
+        inflater.inflate(R.menu.options_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.help:
-
-
+                Log.e("temp-to_implement", "help button pressed");
+                return true;
             case R.id.shop:
+                Log.e("temp-to_implement", "shop button pressed");
                 return true;
             case R.id.profile:
+                Log.e("temp-to_implement", "profile button pressed");
+                profile();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
+    private void profile() {
+        Intent profileIntent = new Intent(LoggedInActivity.this, ProfileActivity.class);
+        startActivity(profileIntent);
+    }
+
 }
