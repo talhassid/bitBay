bc2abe2f0e459a9891e4226a57aa1f5c34749370
roeisun <roeisun77@gmail.com>
2018-04-10 15:51:57 +0300

    remove debug cart condition

--- app/src/main/java/com/bitbay/bitbay/ActivityFilteredCategories.java
+++ app/src/main/java/com/bitbay/bitbay/ActivityFilteredCategories.java
@@ -46,7 +46,7 @@ public class ActivityFilteredCategories extends Activity {
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
                 String userKey = String.valueOf(dataSnapshot.child("userId").getValue());
-                if (userKey.equals(myAccount.getId())) { //TODO: add not
+                if (!userKey.equals(myAccount.getId())) { //TODO: add not
 
                     String price = String.valueOf(dataSnapshot.child("price").getValue());
                     String imagePath = String.valueOf(dataSnapshot.child("storagePath").getValue());
