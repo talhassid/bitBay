c23afb0fc5be0ab32d6b6c147242835e539f82b0
talhassid <talhassidd@gmail.com>
2018-04-19 01:49:40 +0300

    payment activity

--- app/src/main/java/com/bitbay/bitbay/BitcoinPaymentActivity.java
+++ app/src/main/java/com/bitbay/bitbay/BitcoinPaymentActivity.java
@@ -1,116 +1,181 @@
 package com.bitbay.bitbay;
 
+
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.coinbase.android.sdk.OAuth;
-import com.coinbase.api.Coinbase;
-import com.coinbase.api.CoinbaseBuilder;
-import com.coinbase.api.entity.OAuthTokensResponse;
-import com.coinbase.api.exception.CoinbaseException;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 
-import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectView;
-import roboguice.util.RoboAsyncTask;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.InsufficientMoneyException;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionConfidence;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.RegTestParams;
+import org.bitcoinj.params.TestNet3Params;
+import org.bitcoinj.wallet.SendRequest;
+import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.listeners.WalletCoinsReceivedEventListener;
 
-public class BitcoinPaymentActivity extends RoboActivity {
+import java.io.File;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-    private static final String CLIENT_ID = "5c9df3d9ac69af05fc03be0c25e8b75bc578c883f65cd3cb329201f29e29e5d5";
-    private static final String CLIENT_SECRET = "7676804342763d9be5a84296839db19a3117161df4ad22be3a838234a7bc9e51";
-    private static final String REDIRECT_URI = "coinbase-android-example://coinbase-oauth";
+public class BitcoinPaymentActivity extends AppCompatActivity {
 
-    @InjectView(R.id.the_text_view)
-    private TextView mTextView;
+    public WalletAppKit kit;
+    private NetworkParameters params;
+    private Address forwardingAddress;
+    private String filePrefix;
 
-    public class DisplayEmailTask extends RoboAsyncTask<String> {
-        private OAuthTokensResponse mTokens;
+    final Handler handler = new Handler();
+    private String userKey;
+    private EditText mEdit;
 
-        public DisplayEmailTask(OAuthTokensResponse tokens) {
-            super(BitcoinPaymentActivity.this);
-            mTokens = tokens;
-        }
-
-        public String call() throws Exception {
-            Coinbase coinbase = new CoinbaseBuilder().withAccessToken(mTokens.getAccessToken()).build();
-            return coinbase.getUser().getEmail();
-        }
-
-        @Override
-        public void onException(Exception ex) {
-            mTextView.setText("There was an error fetching the user's email address: " + ex.getMessage());
-        }
-
-        @Override
-        public void onSuccess(String email) {
-            mTextView.setText("Success! The user's email address is: " + email);
-        }
-    }
-
-    public class CompleteAuthorizationTask extends RoboAsyncTask<OAuthTokensResponse> {
-        private Intent mIntent;
-
-        public CompleteAuthorizationTask(Intent intent) {
-            super(BitcoinPaymentActivity.this);
-            mIntent = intent;
-        }
-
-        @Override
-        public OAuthTokensResponse call() throws Exception {
-            return OAuth.completeAuthorization(BitcoinPaymentActivity.this, CLIENT_ID, CLIENT_SECRET, mIntent.getData());
-        }
-
-        @Override
-        public void onSuccess(OAuthTokensResponse tokens) {
-            new DisplayEmailTask(tokens).execute();
-        }
-
-        @Override
-        public void onException(Exception ex) {
-            mTextView.setText("There was an error fetching access tokens using the auth code: " + ex.getMessage());
-        }
-    }
+    //HARDCODED
+    private double price = 0.000000000001;
+    private String recipient = "mkVm2g34Mq5Kqx7AWKoMe2ARzotWTM75ov";
+    public TextView mText;
 
     @Override
-    protected void onNewIntent(final Intent intent) {
-        if (intent != null && intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
-            new CompleteAuthorizationTask(intent).execute();
-        }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bitcoin_payment);
+        mText = (TextView) findViewById(R.id.statusBit);
+//        get price from prev activity
+//        Intent intent = getIntent();
+//        Bundle bundle = intent.getExtras();
+//        price = (int) bundle.get("price");
+        mEdit = (EditText) findViewById(R.id.userKey);
+        createForwardingAddress();
+        createNewWallet();
 
-        try {
-            OAuth.beginAuthorization(this, CLIENT_ID, "user", REDIRECT_URI, null);
-        } catch (CoinbaseException ex) {
-            mTextView.setText("There was an error redirecting to Coinbase: " + ex.getMessage());
-        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.bitcoin_menu, menu);
-        return true;
+    public void getKey(View view){
+        userKey = mEdit.getText().toString();
+        mEdit.setText("");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void createForwardingAddress(){
+        String network = "testnet";
+        params = TestNet3Params.get();
+        filePrefix = "forwarding-service-testnet";
+        forwardingAddress = Address.fromBase58(params, recipient);
+        mText.setText("finish step: create forwarding address");
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    class AsyncWallet extends AsyncTask{
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            File file = new File(getApplicationContext().getFilesDir().getPath().toString());
+            kit = new WalletAppKit(params, file, filePrefix) {
+                @Override
+                protected void onSetupCompleted() {
+                    if (wallet().getKeyChainGroupSize() < 1)
+                        wallet().importKey(new ECKey());
+                }
+
+
+            };
+//        if (params == RegTestParams.get()) {
+//            kit.connectToLocalHost();
+//        }
+            mText.setText("finish step: create wallet");
+            // Download the block chain and wait until it's done.
+            kit.startAsync();
+            kit.awaitRunning();
+            mText.setText("finish step: Download the block chain");
+            return null;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(Object o) {
+            walletListeners();
+        }
+    }
+    public void createNewWallet() {
+        new AsyncWallet().execute();
+    }
+
+    void walletListeners() {
+        kit.wallet().addCoinsReceivedEventListener(new WalletCoinsReceivedEventListener() {
+            @Override
+            public void onCoinsReceived(Wallet w, Transaction tx, Coin prevBalance, Coin
+                    newBalance) {
+                // Runs in the dedicated "user thread".
+            }
+        });
+        kit.wallet().addCoinsReceivedEventListener(new WalletCoinsReceivedEventListener() {
+            @Override
+            public void onCoinsReceived(Wallet w, Transaction tx, Coin prevBalance, Coin
+                    newBalance) {
+                Coin value = tx.getValueSentToMe(w);
+                System.out.println("Received tx for " + value.toFriendlyString() + ": " + tx);
+                System.out.println("Transaction will be forwarded after it confirms.");
+
+                Futures.addCallback(tx.getConfidence().getDepthFuture(1), new
+                        FutureCallback<TransactionConfidence>() {
+                            @Override
+                            public void onSuccess(TransactionConfidence result) {
+                                forwardCoins(result);
+                            }
+
+                            @Override
+                            public void onFailure(Throwable t) {
+                            }
+                        });
+            }
+        });
+
+    }
+
+    public void forwardCoins(TransactionConfidence tx) {
+
+    }
+
+    /***
+     * Then we just invoke a method we define ourselves called forwardCoins when the transaction
+     * that sends us money confirms.
+     * @param tx
+     */
+    public void sendCoins(Transaction tx) throws InsufficientMoneyException {
+        Coin value = tx.getValueSentToMe(kit.wallet());
+        System.out.println("Forwarding " + value.toFriendlyString() + " BTC");
+        final Coin amountToSend = value.subtract(Transaction.REFERENCE_DEFAULT_MIN_TX_FEE);
+        final Wallet.SendResult sendResult = kit.wallet().sendCoins(kit.peerGroup(),
+                forwardingAddress, amountToSend);
+        System.out.println("Sending ...");
+
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Sent coins onwards! Transaction hash is " + sendResult.tx
+                        .getHashAsString());
+            }
+        } ;
+        sendResult.broadcastComplete.addListener(run, Executors.newSingleThreadExecutor());
+    }
+
+
+    public Transaction createTransaction(Address address, Coin value) throws
+            InsufficientMoneyException {
+        SendRequest req = SendRequest.to(address, value);
+        req.feePerKb = Coin.parseCoin("0.0005");
+        Wallet.SendResult result = kit.wallet().sendCoins(kit.peerGroup(), req);
+        Transaction createdTx = result.tx;
+        return createdTx;
     }
 }
