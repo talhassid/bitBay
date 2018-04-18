3353e9a26e56f9a96ffccea1276f187802cd3fc2
talhassid <talhassidd@gmail.com>
2018-04-18 17:33:36 +0300

    bitcoin payment

---
+++ app/src/main/java/com/bitbay/bitbay/BitcoinValueActivity.java
@@ -0,0 +1,113 @@
+package com.bitbay.bitbay;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class BitcoinValueActivity extends AppCompatActivity {
+
+    public static final String BPI_ENDPOINT = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    private OkHttpClient okHttpClient = new OkHttpClient();
+    private ProgressDialog progressDialog;
+    private TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bitcoin);
+        txt = (TextView) findViewById(R.id.txt);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("BPI Loading");
+        progressDialog.setMessage("Wait ...");
+        Button loadButton = (Button)findViewById(R.id.action_load2);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.load_item, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_load) {
+            load(null);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void load(View view) {
+        Request request = new Request.Builder()
+                .url(BPI_ENDPOINT)
+                .build();
+
+        progressDialog.show();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(BitcoinValueActivity.this, "Error during BPI loading : "
+                        + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response)
+                    throws IOException {
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        parseBpiResponse(body);
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void parseBpiResponse(String body) {
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            JSONObject jsonObject = new JSONObject(body);
+            JSONObject timeObject = jsonObject.getJSONObject("time");
+            builder.append(timeObject.getString("updated")).append("\n\n");
+
+            JSONObject bpiObject = jsonObject.getJSONObject("bpi");
+            JSONObject usdObject = bpiObject.getJSONObject("USD");
+            builder.append(usdObject.getString("rate")).append("$").append("\n");
+
+            JSONObject gbpObject = bpiObject.getJSONObject("GBP");
+            builder.append(gbpObject.getString("rate")).append("£").append("\n");
+
+            JSONObject euroObject = bpiObject.getJSONObject("EUR");
+            builder.append(euroObject.getString("rate")).append("€").append("\n");
+
+            txt.setText(builder.toString());
+
+        } catch (Exception e) {
+
+        }
+    }
+
+}
--- app/src/main/java/com/bitbay/bitbay/PaypalActivity.java
+++ app/src/main/java/com/bitbay/bitbay/PaypalActivity.java
@@ -11,8 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
@@ -20,14 +18,14 @@ import com.paypal.android.sdk.payments.PaymentActivity;
 import com.paypal.android.sdk.payments.PaymentConfirmation;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 
 public class PaypalActivity extends AppCompatActivity {
 
     TextView m_response;
     PayPalConfiguration m_configuration;
     //the id is the link to the paypal account
-    String m_paypalClientId = "ATom87_nHy9ovxAQo1Yv4q8nwfHHVwwT8w13LwX8eDf22v0BGrYYsoRdw5n9S7BTtvEVFsHHHewh6Jcu";
+    String m_paypalClientId =
+            "ATom87_nHy9ovxAQo1Yv4q8nwfHHVwwT8w13LwX8eDf22v0BGrYYsoRdw5n9S7BTtvEVFsHHHewh6Jcu";
     Intent m_service;
     int m_paypalRequestCode = 999; //
     int price;
@@ -41,50 +39,66 @@ public class PaypalActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        price = (int)bundle.get("price");
+        price = (int) bundle.get("price");
         myAccount = (GoogleSignInAccount) bundle.get("gAccount");
         String s_price = String.valueOf(price);
         totalCartValue = findViewById(R.id.total_amount);
         totalCartValue.setText("Total cart value is " + s_price + " $");
         m_response = (TextView) findViewById(R.id.response);
-        m_configuration = new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+        m_configuration = new PayPalConfiguration().environment(PayPalConfiguration
+                .ENVIRONMENT_SANDBOX)
                 .clientId(m_paypalClientId);
         m_service = new Intent(this, PayPalService.class);
         m_service.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, m_configuration);
         startService(m_service); //paypal service, listening to calls to paypal app
+
+        Button bitcoinPayment = (Button) findViewById(R.id.bitcoinPayment);
+        Button bitcoinValue = (Button) findViewById(R.id.bitcoinValue);
     }
 
     void pay(View view) {
         PayPalPayment payment = new PayPalPayment(new BigDecimal(price), "USD",
                 "Test payment with paypal", PayPalPayment.PAYMENT_INTENT_SALE);
         Intent intent = new Intent(this, PaymentActivity.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,m_configuration);
-        intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payment);
-        startActivityForResult(intent,m_paypalRequestCode);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, m_configuration);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+        startActivityForResult(intent, m_paypalRequestCode);
+    }
+
+    void moveToBitCoinPayment(View view) {
+        Intent bitCoinIntent = new Intent(PaypalActivity.this, BitcoinPaymentActivity.class);
+        bitCoinIntent.putExtra("price", price);
+        startActivity(bitCoinIntent);
+    }
+
+    void moveToBitCoinValue(View view) {
+        Intent bitCoinIntent = new Intent(PaypalActivity.this, BitcoinValueActivity.class);
+        bitCoinIntent.putExtra("price", price);
+        startActivity(bitCoinIntent);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == m_paypalRequestCode){
-            if (resultCode == Activity.RESULT_OK){
-                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
-                if (confirmation != null){
+        if (requestCode == m_paypalRequestCode) {
+            if (resultCode == Activity.RESULT_OK) {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity
+                        .EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null) {
                     String state = confirmation.getProofOfPayment().getState();
-                    if (state.equals("approved")){
+                    if (state.equals("approved")) {
                         m_response.setText("payment approved");
-                        Toast.makeText(getApplication(), "Payment Approved", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplication(), "Payment Approved", Toast.LENGTH_SHORT)
+                                .show();
 
                         Intent cartIntent = new Intent(this, ProfileActivity.class);
-                        cartIntent.putExtra("target","cart");
-                        cartIntent.putExtra("account",myAccount);
-                        cartIntent.putExtra("callingSrc","paypal");
+                        cartIntent.putExtra("target", "cart");
+                        cartIntent.putExtra("account", myAccount);
+                        cartIntent.putExtra("callingSrc", "paypal");
                         startActivity(cartIntent);
-                    }
-                    else
+                    } else
                         m_response.setText("error in the payment");
-                }
-                else
+                } else
                     m_response.setText("Confirmation is null");
             }
         }
